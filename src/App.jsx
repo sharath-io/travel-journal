e84ad4c501fcf333c24680fe03eb8ed5c72c50ee
Sharath <sharath.esr@gmail.com>
2025-01-25 18:56:34 +0530

    feat: built header of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
+import Entry from "./components/Entry"
 
 function App() {
-
   return (
     <>
-      
+    <Entry/>    
     </>
   )
 }
---
+++ src/components/Entry.jsx
@@ -0,0 +1,8 @@
+export default function Entry(){
+    return (
+        <header>
+            <img src="src/images/globe.png" alt="globe"/>
+            <h1>my travel journal</h1>
+        </header>
+    )
+}
