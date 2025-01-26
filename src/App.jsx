dbb35f1ab75f3b0b12c725854f0abd43f0ff3180
Sharath <sharath.esr@gmail.com>
2025-01-26 20:45:14 +0530

    used props to send data for reusability of entry component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import './App.css'
 import Entry from "./components/Entry"
 import Header from './components/Header'
+import data from "./data/data";
 
 function App() {
+
+  const entryElements = data.map(entry =>{
+    return (
+      <Entry 
+      key={entry.id}
+      entry={entry}
+      />
+
+    )
+  })
   return (
     <>
       <Header/>   
-      <Entry/> 
+      {entryElements}
+     
+      
     </>
   )
 }
 
-export default App
+export default App;
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,39 +1,28 @@
-export default function Entry() {
+export default function Entry(props) {
   return (
     <article className="entry">
-      
-      <img
-        src="https://scrimba.com/links/travel-journal-japan-image-url"
-        alt="location-photo"
-        className="location-photo"
-      />
+      <div className="main-image-container">
+       <img src={props.entry.img.src} alt={props.entry.img.alt} className="main-image" />
+      </div>
 
       <div className="entry-details">
         <div className="entry-header">
           <img src="src/images/marker.png" alt="location" className="marker" />
-          <h1 className="country-name">japan</h1>
+          <h1 className="country-name">{props.entry.country}</h1>
           <a
             className="google-maps-link"
-            href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+            href={props.entry.googleMapsLink}
             target="_blank"
-         >
+          >
             View on Google Maps
           </a>
         </div>
 
+        <h1 className="location-name">{props.entry.title}</h1>
 
-        <h1 className="location-name">Mount Fauji</h1>
-
-        <p className="date">12 Jan, 2021 - 24 Jan, 2021</p>
-        <p className="location-details">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
-        </p>
-
-
+        <p className="date">{props.entry.dates}</p>
+        <p className="location-details">{props.entry.text}</p>
       </div>
-
     </article>
   );
 }
